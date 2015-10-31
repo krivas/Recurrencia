1859a3598aa0013cf6a593ac89063cd3e1fa095e
Kevin Rivas <krivas.reyes@gmail.com>
2015-10-31 19:10:57 -0400

    Refactor a la clase orden

--- Recurrencia/Model/Order.cs
+++ Recurrencia/Model/Order.cs
@@ -8,8 +8,42 @@ namespace Recurrencia.Model
 {
     class Order
     {
-        Client client;
-        DateTime originDate;
-        DateTime releaseDate;
+        private int _idClient;
+
+        public int IdClient
+        {
+            get { return _idClient; }
+            set { _idClient = value; }
+        }
+        private DateTime _originDate;
+
+        public DateTime OriginDate
+        {
+            get { return _originDate; }
+            set { _originDate = value; }
+        }
+        private DateTime _releaseDate;
+
+        public DateTime ReleaseDate
+        {
+            get { return _releaseDate; }
+            set { _releaseDate = value; }
+        }
+
+       private int _total;
+
+       public int Total
+       {
+           get { return _total; }
+           set { _total = value; }
+       }
+       public Order(int id, DateTime orderDate, DateTime requireDate, int total)
+       {
+           this._idClient = id;
+           this._originDate = orderDate;
+           this._releaseDate = requireDate;
+           this._total = total;
+       }
+       
     }
 }
